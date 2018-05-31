c5ebc8d41648cff5586048a69604e21b41cca894
Neil Leonard <quantumchamploo@gmail.com>
2018-05-30 17:28:35 -0700

    Got Gradient descent to work

--- src/netNode.java
+++ src/netNode.java
@@ -1,16 +1,24 @@
 
 public class netNode {
+	// fields for each node. Act value and bias per node
+	// the wArray contains weights to PREVIOUS arrays
+	// the errArray is the error counter for back prop and gradient descent
 	public double actValue;
 	public double bias;
 	public double[] wArray;
+	public double[] errArray;
 	
 	
 	public netNode(int wLength){ // general constructor with init weight and bias array size 30
 		this.actValue = 0; //should be null?
 		
 		this.wArray = new double[wLength];
+		this.errArray = new double[wLength];
 		for(int i = 0; i < wLength; i++){
-			this.wArray[i] = 0.0;
+			this.wArray[i] = 1.0;
+		}
+		for(int i = 0; i < wLength; i++){
+			this.errArray[i] = 0.0;
 		}
 		this.bias = 0.0;
 		
@@ -22,7 +30,13 @@ public class netNode {
 		
 		this.wArray = new double[30];
 	}
-	
+	// resets the error for each data run
+	public void resetErr(){
+		for(int i = 0; i < this.errArray.length; i++){
+			this.errArray[i] = 0.0;
+		}
+	}
+	// I believe unnecessary
 	public static double sigFun(double input){
 		double result = (1 / ( 1 + Math.exp(-1 * input)) );
 		return result;
