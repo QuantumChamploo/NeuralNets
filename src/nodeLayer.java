57cbf93c45db64ce25c1b88d54d33c7022565680
Neil Leonard <quantumchamploo@gmail.com>
2018-05-30 17:29:32 -0700

    Got grad descent to work

--- src/nodeLayer.java
+++ src/nodeLayer.java
@@ -1,25 +1,63 @@
 
 public class nodeLayer {
+
+	/**
+	 * fields for the nodeLayer class. A reference to the previous layer and next layer.
+	 * Nodes are held in an array. We will also keep the weighted input and the error in an
+	 * array. Additionally the sum of the partial derivatives will be kept in an array as well
+	 */
 	public nodeLayer prevLayer;
 	public nodeLayer nextLayer;
 	public netNode[] nodes;
 	public double[] wInput;
 	public double[] error;
-	
-	
+
+	public double[] derSumB;
+
+
+	/**
+	 * the constructor for the node layer class. Need to specify the number of nodes and the number
+	 * of nodes in the prev layer so the number of weights is correct
+	 *
+	 * @param length number of nodes
+	 * @param wLength number of nodes in prev layer to figure out the number of weights
+     */
 	public nodeLayer(int length, int wLength){
 		this.prevLayer = null;
 		this.nextLayer = null;
 		this.nodes = new netNode[length];
 		this.wInput = new double[length];
 		this.error = new double[length];
+
+		this.derSumB = new double[length];
+
 		for(int i = 0; i < length; i++){
 			this.nodes[i] = new netNode(wLength);
 		}
+		// reset the counter fields
+		this.resetSums();
 	}
-	
+
+	public void resetSums(){
+		for(int i = 0; i < this.derSumB.length; i++){
+			// make every sum to zero
+			this.derSumB[i] = 0.0;
+		}
+		for(int i = 0; i < this.nodes.length; i++){
+			//go into each node and do the same
+			this.nodes[i].resetErr();
+		}
+	}
+
+	/**
+	 * helper method that calculates the weighted input from one node to another
+	 *
+	 * @param curr the reciever node int location in the nodes array
+	 * @param prev the input node int location in the prev array
+     * @return
+     */
 	public double weightedInput(int curr, int prev){
-		
+
 		double actVal = this.prevLayer.nodes[prev].actValue;
 		
 		double wVal = this.nodes[curr].wArray[prev];
@@ -28,10 +66,16 @@ public class nodeLayer {
 		return ((actVal * wVal) + bVal);
 		
 	}
-	
+
+	/**
+	 * helper method that updates the layers wInput field with the weighted input
+	 * to each node
+	 */
 	public void wResults(){
+		// outer loop to go through each node
 		for(int i = 0; i < this.nodes.length; i++){
 			double results = 0.0;
+			// inner loop to add up all the weighted input
 			for(int j = 0; j < this.prevLayer.nodes.length; j++){
 				if(this.nodes[i] != null){
 					results += weightedInput(i, j);
@@ -41,24 +85,62 @@ public class nodeLayer {
 			this.wInput[i] = results;
 		}
 	}
+
+	/**
+	 * Static method to forward prop the layer
+	 */
 	public void forProp(){
 		this.wResults();
 		for(int i = 0 ; i < this.nodes.length; i++){
 			 
 			this.nodes[i].actValue = sigFun(this.wInput[i]);
 		}
-	
-		
 		
 	}
-	
+
+	/**
+	 * corner case of back propagation. This is for the last layer, which
+	 * is to be compared to answers give
+	 *
+	 * @param answers an array of correct answers for the datum
+     */
+	public void bckProp1(double[] answers){
+		for(int i = 0; i < this.nodes.length; i++){
+			this.error[i] = this.error1(answers[i], i);
+		}
+
+	}
+
+	/**
+	 * the main version of back propagation, to move error from one layer to
+	 * its prev layer
+	 */
+	public void bckProp2(){
+		for(int i = 0; i < this.nodes.length; i++){
+			this.error[i] = this.error2(i);
+
+		}
+	}
+
+	/**
+	 * The sigmoid function method
+	 * @param input
+	 * @return
+     */
 	public static double sigFun(double input){
 		double result = (1 / ( 1 + Math.exp(-1 * input)) );
 		return result;
 	}
+
+	/**
+	 * the derivative of the sigmoid function
+	 * @param input
+	 * @return
+     */
 	public static double sigPrime(double input){
 		double result = (Math.exp(-1 * input) / ( 1 + Math.exp(-1 * input)) );
 		return result;
+
 	}
 	/**
 	 * error1 is a method to calculate the error of the outer node, which
@@ -84,7 +166,11 @@ public class nodeLayer {
 		}
 		return result;
 	}
-	
+
+	/**
+	 * toString method for printing a layer in a readable way
+	 * @return
+     */
 	public String toString(){
 		String results = "[";
 		for(int i = 0; i < this.nodes.length; i++){
